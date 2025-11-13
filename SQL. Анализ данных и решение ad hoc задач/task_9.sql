e921f3a22e14ad837923bfdeda68b7afa3b4ed48
mariapanikarevic <mariapanikarevic@gmail.com>
2025-11-13 15:08:12 +0300

    Update task_9.sql

--- SQL. Анализ данных и решение ad hoc задач/task_9.sql
+++ SQL. Анализ данных и решение ad hoc задач/task_9.sql
@@ -1,1 +1,9 @@
-
+SELECT COUNT(date) AS data_count,
+       COUNT(time) AS data_time,
+       COUNT(amount) AS data_amount,
+       COUNT(seller_id) AS data_seller_id
+FROM fantasy.events
+WHERE date IS NULL
+  OR time IS NULL
+  OR amount IS NULL
+  OR seller_id IS NULL;
