51deb4b709ddb478493efe4e7709e47cd3371643
mariapanikarevic <mariapanikarevic@gmail.com>
2025-11-13 15:04:39 +0300

    Update task_4.sql

--- SQL. Анализ данных и решение ad hoc задач/task_4.sql
+++ SQL. Анализ данных и решение ad hoc задач/task_4.sql
@@ -1,1 +1,9 @@
-
+SELECT COUNT(*)
+FROM fantasy.users
+WHERE class_id IS NULL
+      AND ch_id IS NULL
+      AND pers_gender IS NULL
+      AND server IS NULL
+      AND race_id IS NULL
+      AND payer IS NULL
+      AND loc_id IS NULL;
