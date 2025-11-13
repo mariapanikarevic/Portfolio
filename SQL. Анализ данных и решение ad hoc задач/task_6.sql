e60ca07694c0de3852c36ffad4d59728a047e868
mariapanikarevic <mariapanikarevic@gmail.com>
2025-11-13 15:07:19 +0300

    Update task_6.sql

--- SQL. Анализ данных и решение ad hoc задач/task_6.sql
+++ SQL. Анализ данных и решение ad hoc задач/task_6.sql
@@ -1,1 +1,10 @@
-
+SELECT c.table_schema,
+       c.table_name,
+       c.column_name,
+       c.data_type,
+       k.constraint_name
+FROM information_schema.columns AS c 
+LEFT JOIN information_schema.key_column_usage AS k 
+    USING(table_name, column_name, table_schema)
+WHERE c.table_schema = 'fantasy' AND c.table_name = 'events'
+ORDER BY c.table_name;
