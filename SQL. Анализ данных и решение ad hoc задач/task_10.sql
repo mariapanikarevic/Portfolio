6f0fa7a1e4c1e42bd8d0a58585464b018cca4fb0
mariapanikarevic <mariapanikarevic@gmail.com>
2025-11-13 15:08:41 +0300

    Update task_10.sql

--- SQL. Анализ данных и решение ad hoc задач/task_10.sql
+++ SQL. Анализ данных и решение ad hoc задач/task_10.sql
@@ -1,1 +1,14 @@
-
+-- Доля платящих пользователей по всем данным:
+   SELECT COUNT(id) AS count_id,
+       SUM(payer) AS payer_id,
+       AVG(payer) AS avg_payer
+   FROM fantasy.users;
+-- Доля платящих пользователей в разрезе расы персонажа:
+   SELECT DISTINCT u.race_id,
+        r.race,
+        SUM(u.payer) AS count_payer,
+        ROUND(AVG(u.payer), 3) AS payer_share
+   FROM fantasy.users AS u
+   LEFT JOIN fantasy.race AS r ON u.race_id = r.race_id
+   GROUP BY u.race_id, r.race
+   ORDER BY count_payer DESC;
